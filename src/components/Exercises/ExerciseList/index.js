7341165085cd6341b890e9cd8b122fe90c7e6f98
dandenney <dd.subscriptions@gmail.com>
2017-07-20 22:48:28 -0400

    feat: Add an ExerciseList for reals

---
+++ src/components/Exercises/ExerciseList/index.js
@@ -0,0 +1,16 @@
+import { h, Component } from 'preact';
+
+export default class extends Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<ul>
+				<li>Exercise</li>
+				<li>Exercise</li>
+			</ul>
+		);
+	}
+}
--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,5 +1,6 @@
 import { h, Component } from 'preact';
 import { auth, database } from '../firebase';
+import ExerciseList from './ExerciseList';
 import SignIn from '../SignIn';
 
 export default class Exercises extends Component {
@@ -7,6 +8,7 @@ export default class Exercises extends Component {
 		return (
 			<section>
 				<SignIn />
+				<ExerciseList />
 			</section>
 		);
 	}

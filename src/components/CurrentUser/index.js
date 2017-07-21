ba9f3e2bee1f716f6ca597c7246049686e184fdd
dandenney <dd.subscriptions@gmail.com>
2017-07-21 11:24:43 -0400

    feat: Add an avatar for signed in users

--- src/components/CurrentUser/index.js
+++ src/components/CurrentUser/index.js
@@ -6,9 +6,10 @@ export default class CurrentUser extends Component {
 	}
 
 	render() {
+		const user = this.props.user;
 		return (
 			<article>
-				<img alt="Placeholder" src="http://placehold.it/80x80" />
+				<img alt={user.displayName} src={user.photoURL} width="40" />
 				<button>Sign Out</button>
 			</article>
 		);

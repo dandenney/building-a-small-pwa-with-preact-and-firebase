68888063a715ac0d9f55fa4578e43bdfe6977321
dandenney <dd.subscriptions@gmail.com>
2017-07-23 11:39:00 -0400

    feat: Add the value of the name input to local state

--- src/components/Exercises/NewExercise/index.js
+++ src/components/Exercises/NewExercise/index.js
@@ -3,9 +3,39 @@ import { h, Component } from 'preact';
 export default class NewExercise extends Component {
 	constructor() {
 		super();
+
+		this.state = {
+			name: ''
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
 	}
 
 	render() {
-		return 'New Exercise Form';
+		const name = this.state;
+		return (
+			<section>
+				<h2>New Exercise</h2>
+
+				<form>
+					<div>
+						<label for="name">Name</label>
+						<input
+							type="text"
+							name="name"
+							onChange={this.handleChange}
+							placeholder="Chest Press"
+							value={this.state.name}
+						/>
+					</div>
+				</form>
+			</section>
+		);
 	}
 }
